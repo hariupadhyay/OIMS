8a7429cfac9ccb9233ada063bb4dc4618853d6c1
hari shankar upadhyay <hariupadhyay95@gmail.com>
2017-07-19 15:08:17 +0530

    Admin Controller are Created

---
+++ oims/src/main/java/com/goodtech/oims/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.goodtech.oims.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value={"/","/home","index"})
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("test","welcome to spring");
+		
+		return mv;
+		
+		
+	}
+
+}
